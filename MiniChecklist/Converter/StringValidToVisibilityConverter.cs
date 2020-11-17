06161f6cd4eb46b31bc5202c34fc4a5bfd7d9137
Gustice <Jakob.Ger@gmail.com>
2020-11-17 22:36:25 +0100

    feature: Expand button only visible if description available

---
+++ MiniChecklist/Converter/StringValidToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MiniChecklist.Converter
+{
+    public class StringValidToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = (string)value;
+            var constraint = (string)parameter;
+
+            if (constraint.Equals("NotEmpty"))
+            {
+                if (string.IsNullOrEmpty(text) )
+                    return Visibility.Collapsed;
+
+                return Visibility.Visible;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
